48c80264fb1bc52803f33499577270c1c96c12ac
Manuel CH <manuel_7795@hotmail.com>
2021-11-07 21:26:15 -0600

    Include new route "test"

--- src/routes/v1/problem.ts
+++ src/routes/v1/problem.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 
-import { submit, validate } from '../../controllers/problem';
+import { submit, test, validate } from '../../controllers/problem';
 import authorization from '../../middleware/authorization';
 
 const router = Router();
 
 router.post('/validate', authorization, validate);
 router.post('/submit', authorization, submit);
+router.post('/test', authorization, test);
 
 export default router;
